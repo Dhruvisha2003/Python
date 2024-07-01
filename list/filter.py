eebcf48309cb4a023d11a1c8d17e1ab60428ee52
naitik <naitikkherala47@gmail.com>
2024-07-01 16:50:47 +0530

    updated

--- list/filter.py
+++ list/filter.py
@@ -1,25 +1,7 @@
 str_list = ['he', 'dr', 'dh', 'ji', 'd', 'rt', 'hr', 6, 7, 5]
-
 new_list = []
 i = 0
-while i < len(str_list):
-    current_str = str_list[i]
-    if isinstance(current_str, str) and current_str[0] == 'd':
-        new_list.append(current_str)
-    i = i + 1
-
+for i in str_list :
+    if type(i) == str and i[0] == 'd' :
+        new_list.append(i)
 print(new_list)
-
-
-
-
-# new_list = []
-# i = 0
-# while i < len(str_list) :
-#     current_str = str_list[i]
-#     if current_str[0] != 'd' :
-#         str_list.remove(str_list[i])
-#         # new_list.append(str_list)
-#     i = i + 1
-
-# print(str_list)
