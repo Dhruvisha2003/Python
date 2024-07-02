f179fbfab5a33ed6d44022cd1e3337bf58041ba8
naitik <naitikkherala47@gmail.com>
2024-07-02 17:53:46 +0530

    updated

---
+++ dictionary/practice.py
@@ -0,0 +1,16 @@
+s1 = {'Name':'Dhruvisha','Surname':'Vekariya','Number':'986469'}
+# s1 = dict({'Name':'Dhruvisha','Course':'Python'})
+# s1 = dict([('Name','Dhruvisha'),('Course','Python')])
+# s1 = {'Name':'Dhruvisha','Number':[12,34,56,67,87]}
+# for i in s1.items() :
+#     # print(i ,':',s1[i])
+#     print(i[0],':',i[1])
+
+s1['Course'] = 'Python'
+s1.update({'Fees':'60000'})
+print(s1)
+# print(s1.get('Name'))
+# print(s1['Name'])
+# print(s1.keys())
+# print(s1.values())
+# print(s1.items())
