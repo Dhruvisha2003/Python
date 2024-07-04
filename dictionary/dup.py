fe85853aa174bdd877a1f37028719f629d26f0e3
naitik <naitikkherala47@gmail.com>
2024-07-04 17:46:46 +0530

    Updated

--- dictionary/dup.py
+++ dictionary/dup.py
@@ -1,9 +1,16 @@
-dict1 = {'b':67, 'r':90, 'q':8, 'a':56, 't':14, 'e':8, 'y':67}
-my_list = list(dict1.values())
-new_dict = {}
-for value in my_list:
-    if value != new_dict.values():
-        new_dict[value] = value
+dict1 = {'b': 67, 'r': 90, 'q': 8, 'a': 56, 't': 14, 'e': 8, 'y': 67}
 
-new_list = list(new_dict.values())
-print(new_list)
+set1 = set()
+
+list1= []
+for key, value in dict1.items():
+    if value in set1:
+        print(value)
+        list1.append(key) 
+    else:
+        set1.add(value) 
+
+# for key in list1:
+#     del dict1[key]
+
+print(dict1)
