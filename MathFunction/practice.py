a50b2eeee0d6c0b5015e33b58f5ec795ec802d81
naitik <naitikkherala47@gmail.com>
2024-07-08 17:48:29 +0530

    Updated

---
+++ MathFunction/practice.py
@@ -0,0 +1,16 @@
+import math
+ 
+num = [6,87,3,90,34]
+# num = 56.8
+# print(num)
+# print(min(num))
+# print(max(num))
+# print(abs(-7.7))
+# print(pow(4,2)) # 4*4
+
+# print(math.sqrt(25))  #Squear Root
+# print(math.ceil(34.1))
+# print(math.floor(56.9))
+# print(math.factorial(5))
+# print(math.pi)
+
